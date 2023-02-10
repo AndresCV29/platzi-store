8b18294b9d6275b92f88a3f931a06905a86344ce
Andres D. Cogollo V <adcv980318@gmail.com>
2023-02-10 09:09:15 -0500

    Snapshots

--- src/__test__/components/Footer.test.js
+++ src/__test__/components/Footer.test.js
@@ -2,6 +2,7 @@
 import 'jsdom-global/register';
 import React from 'react';
 import { mount } from 'enzyme';
+import { create } from 'react-test-renderer';
 import Footer from '../../components/Footer';
 
 describe('<Footer/>', () => {
@@ -12,5 +13,12 @@ describe('<Footer/>', () => {
 
   test('Render del titulo', () => {
     expect(footer.find('.Footer-title').text()).toEqual('Platzi Store');
+  });
+});
+
+describe('Footer snapshot', () => {
+  test('Comprobar la UI del componente Footer', () => {
+    const footer = create(<Footer />);
+    expect(footer.toJSON()).toMatchSnapshot();
   });
 });
--- src/__test__/components/Header.test.js
+++ src/__test__/components/Header.test.js
@@ -2,6 +2,7 @@
 import 'jsdom-global/register';
 import React from 'react';
 import { mount, shallow } from 'enzyme';
+import { create } from 'react-test-renderer';
 import ProviderMock from '../../__mocks__/ProviderMock';
 import Header from '../../components/Header';
 
@@ -21,5 +22,16 @@ describe('<Header />', () => {
       </ProviderMock>,
     );
     expect(header.find('.Header-title').text()).toEqual('Platzi Store');
+  });
+});
+
+describe('Header snapshot', () => {
+  test('Comprobar el snapshot de Header', () => {
+    const header = create(
+      <ProviderMock>
+        <Header />
+      </ProviderMock>,
+    );
+    expect(header.toJSON()).toMatchSnapshot();
   });
 });
